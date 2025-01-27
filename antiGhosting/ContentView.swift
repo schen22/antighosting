4ebb3417fe8852e0bddaa22985db9b9748ecc713
schen22 <sarahchen22@gmail.com>
2025-01-26 19:57:27 -0800

    Initial Commit

---
+++ antiGhosting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  antiGhosting
+//
+//  Created by Sarah Chen on 1/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ antiGhosting/antiGhostingApp.swift
@@ -0,0 +1,17 @@
+//
+//  antiGhostingApp.swift
+//  antiGhosting
+//
+//  Created by Sarah Chen on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct antiGhostingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
